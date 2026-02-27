374e517ce5340a8aaba636e2c914d723abcf578a
Lebarbote <leticiab.teixeira@yahoo.com.br>
2026-02-27 15:40:58 -0300

    estrutura base vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+import Navbar from '@/components/Navbar.vue'
+import Toast from '@/components/Toast.vue'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.initializeAuth()
+})
+</script>
+
+<template>
+  <div class="min-h-screen bg-dark-950">
+    <Navbar />
+    <main class="container mx-auto px-4 py-8">
+      <RouterView />
+    </main>
+    <Toast />
+  </div>
+</template>
